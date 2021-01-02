503e1322f679e1788a7997f725e79a6ab3fe084f
Tomasz Kaliszuk <tomasz.kaliszuk@espeo.eu>
2021-01-02 23:45:14 +0100

    sec commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello World2 - after CD!');
+  res.write('Hello World3 - after CD!');
   console.log('request!');
   res.end();
 }).listen(8080, '0.0.0.0');
