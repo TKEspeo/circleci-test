6b38a04c21abf6cc84e91bda5c49df735789762e
Tomasz Kaliszuk <tomasz.kaliszuk@espeo.eu>
2021-01-04 22:06:30 +0100

    logic with tests

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const http = require('http');
+// const http = require('http');
+//
+// http.createServer(function (req, res) {
+//   res.write('Hello World3 - after CD!');
+//   console.log('request!');
+//   res.end();
+// }).listen(8080, '0.0.0.0');
 
-http.createServer(function (req, res) {
-  res.write('Hello World3 - after CD!');
-  console.log('request!');
-  res.end();
-}).listen(8080, '0.0.0.0');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.post('/', (req, res) => {
+  res.send({ msg: 'hello world from post metho2d' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports = app
---
+++ tests/sample.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest')
+const app = require('../app.js')
+describe('Post Endpoint', () => {
+  it('shoud create new post', async (done ) => {
+    const res = await request(app)
+      .post('/')
+      .send({
+        userId: 1,
+        title: 'test works',
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toHaveProperty('msg')
+    done();
+  })
+
+  it('shoud create new post', async ( done ) => {
+    const res = await request(app)
+      .post('/')
+      .send()
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.msg).toEqual('hello world from post metho2d')
+    done()
+  })
+})
+
+afterAll(async (done) => {
+  done();
+});
